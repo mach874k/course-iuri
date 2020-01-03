50add6cf888b0a226186e314864f7ddf31c1f50a
Iuri Severo <nobreiuri@gmail.com>
2020-01-02 22:01:24 -0300

    Moss Giant setup

---
+++ Dungeon Escape/Assets/Scripts/Enemy/MossGiant.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiant : Enemy
+{
+    private SpriteRenderer _sprite;
+    private Animator _anim;
+    private Vector3 currentTarget;
+    private void Start()
+    {
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public override void Update()
+    {        
+        if(!_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")){
+            Movement();
+        }
+    }
+
+    private void Movement(){
+        if(currentTarget == pointB.position){
+            _sprite.flipX = false;
+        }
+        else{
+            _sprite.flipX = true;
+        }
+
+        if(transform.position == pointA.position){
+            Debug.Log("To no A!");
+            _anim.SetTrigger("Idle");
+            currentTarget = pointB.position;
+        }
+        else if(transform.position == pointB.position){
+            Debug.Log("To no B!");
+            _anim.SetTrigger("Idle");
+            currentTarget = pointA.position;
+        }
+        Debug.Log(currentTarget);
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+}
---
+++ Dungeon Escape/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    private bool _resetJump = false;
+    private bool _grounded = false;
+    [SerializeField]
+    private float _speed = 2.5f;
+    private PlayerAnimation _playerAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if(Input.GetMouseButtonDown(0) && _grounded){
+            _playerAnim.Attack();
+        }
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");  
+        isGrounded();
+
+        if(move != 0)
+            _playerAnim.Flip(move);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded){
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(ResetJumpRoutine());
+            _playerAnim.Jump(true);
+        }
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+        _playerAnim.Move(move);
+    }
+
+    void isGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
+        if (hitInfo.collider != null){
+            if(!_resetJump){
+                _playerAnim.Jump(false);
+                _grounded = true;
+                return;
+            }
+        }
+        _grounded = false;
+        return;
+    }
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
