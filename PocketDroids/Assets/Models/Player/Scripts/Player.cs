dfe1afb9e8fb871ec1db09b0d7e1a7f693fff4a0
Iuri Severo <nobreiuri@gmail.com>
2020-01-28 01:11:16 -0300

    add player, druid, singleton and druid factory

---
+++ PocketDroids/Assets/Models/Player/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	[SerializeField] private int xp = 0;
+	[SerializeField] private int requiredXP = 100;
+	[SerializeField] private int levelBase = 100;
+	[SerializeField] private int level = 1;
+	[SerializeField] private List<GameObject> droids = new List<GameObject>();
+
+	private void Start() {
+		InitLevelData();
+	}
+	public int Xp {
+		get { return xp; }
+	}
+
+	public int RequiredXP {
+		get { return requiredXP; }
+	}
+
+	public int Level {
+		get { return level; }
+	}
+
+	public List<GameObject> Droids {
+		get { return droids; }
+	}
+
+	public void AddXP(int xp){
+		this.xp = Mathf.Max(0, xp);
+	}
+
+	public void AddDroids(GameObject droid){
+		droids.Add(droid);
+	}
+
+	private void InitLevelData(){
+		level = (xp / levelBase + 1);
+		requiredXP = levelBase * level;
+	}
+
+}
---
+++ PocketDroids/Assets/Utilities/DroidFactory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class DroidFactory : Singleton<DroidFactory>
+{
+    [SerializeField] private Droids[] availableDroids;
+    [SerializeField] private Player player;    
+    [SerializeField] private float waitTime = 180.0f;
+    [SerializeField] private int startingDroids = 5;
+    [SerializeField] private float minRange= 5.0f;
+    [SerializeField] private float maxRange = 50.0f;
+    private void Awake() {
+        Assert.IsNotNull(availableDroids);
+        Assert.IsNotNull(player);
+    }
+
+    private void Start() {
+        for(int i=0; i< startingDroids; ++i){
+            InstantiateDroid();
+        }
+        StartCoroutine(GenerateDroids());
+    }
+
+    private IEnumerator GenerateDroids(){
+        while(true){
+            InstantiateDroid();
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private void InstantiateDroid(){
+        int index = Random.Range(0, availableDroids.Length);
+        float x = player.transform.position.x + GenerateRange();
+        float y = player.transform.position.y;
+        float z = player.transform.position.z + GenerateRange();
+
+        Instantiate(availableDroids[index], new Vector3(x, y, z), Quaternion.identity);
+    }
+
+    private float GenerateRange(){
+        float randomNum = Random.Range(minRange, maxRange);
+        bool isPositive = Random.Range(0, 10) < 5;
+        return randomNum * (isPositive? 1 : -1);
+    }
+}
